00ac589a5c423256154bd646ccf0f4226ea222f6
Michael Drotar <michaeldrotar@gmail.com>
2011-06-18 01:07:45 -0400

    BetterCupid's first setting!! :D
    Initial work on handling settings from options.html. It's not pretty,
    but it works. Setting allows users to hide the "You might like" section
    on the homepage.

---
+++ scripts/misc.js
@@ -0,0 +1,19 @@
+var db = core.db;
+
+core.HookEvent("VARIABLES_LOADED", function() {
+	var settings = db.get("settings") || {};
+	
+	core.RegisterLoadingProcess("misc", function() {
+		
+		if ( settings.hideYouMightLike ) {
+			$(".section.matches").each(function() {
+				this.getElementsByTagName("h5").forEach(function(el) {
+					if ( el.innerHTML && el.innerHTML.toLowerCase().indexOf("you might like") >= 0 ) {
+						el.parentNode.style.display = 'none';
+					}
+				});
+			});
+		}
+		
+	});
+});
