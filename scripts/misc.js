5b64e4f6c4a343a428f9ab71cf99d6a395edd81b
Michael Drotar <michaeldrotar@gmail.com>
2011-06-18 02:54:11 -0400

    Recently Visited is now Better than Okay
    The Recently Visited list now tracks a full history of visited users and
    can display up to 10 rows (adjusted via options.html). Up to 2.5 rows are
    visible at a time, the scroll wheel will scroll through the list to view
    the rest.
    
    Also, various minor code edits, and excuse the huge block of comments in
    user_tracker.js -- I'll clean it up soon.

--- scripts/misc.js
+++ scripts/misc.js
@@ -15,5 +15,64 @@ core.HookEvent("VARIABLES_LOADED", function() {
 			});
 		}
 		
+		if ( true ) {
+			var recentlyVisited = document.getElementById("side_recently_viewed");
+			if ( recentlyVisited ) {
+				var numRows = settings.numRecentlyVisitedRows,
+					visits = core.db.get("visits");
+				if ( numRows === 0 || visits === null ) {
+					recentlyVisited.style.display = "none";
+				} else {
+					core.ProcessElement(recentlyVisited, function() {
+						var list = recentlyVisited.getElementByTagName("ul");
+						$(recentlyVisited).find("li,p").detach();
+						for ( var i = 0, l = Math.min(visits.length, numRows*3); i < l; i++ ) {
+							var v = visits[i];
+							var li = document.createElement("li");
+							li.appendTo(list);
+							var a = document.createElement("a");
+							a.href = v.url;
+							a.appendTo(li);
+							var img = document.createElement("img");
+							img.src = v.image;
+							img.border = 0;
+							img.width = 40;
+							img.height = 40;
+							img.appendTo(a);
+						}
+						var holder = document.createElement("div");
+						holder.appendTo(list.parentNode);
+						list.appendTo(holder);
+						
+						if ( list.getHeight() > 135 ) {
+							holder.style.height = "135px";
+							holder.style.overflow = "hidden";
+							holder.style.position = "relative";
+							list.style.position = "absolute";
+							
+							holder.addEventListener("mousewheel", function(e) {
+								var dir = e.wheelDelta > 0 ? 1 : -1,
+									change = dir * Math.ceil(holder.getHeight()/2),
+									height = list.getHeight(),
+									area = height - holder.getHeight(),
+									top = list.getTop(holder);
+									newTop = top + change;
+								
+								if ( dir > 0 && top === 0 ) return;
+								if ( dir < 0 && top === -area ) return;
+								
+								if ( newTop > 0 ) newTop = 0;
+								else if ( newTop < -area ) newTop = -area;
+								
+								list.style.marginTop = newTop + "px";
+								e.stopPropagation();
+								e.preventDefault();
+							});
+						}
+					});
+				}
+			}
+		}
+		
 	});
 }, true);
