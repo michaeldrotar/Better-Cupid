0dc57fd066232f16145105c11e92a15b19762708
Michael <michaeldrotar@gmail.com>
2015-03-10 21:14:23 -0400

    Drawer improvements - trigger recognition and keys
    When a drawer is opened, all its triggers will now also receive the
    .is-open class so they may be styled differently
    
    The data-drawer-key attribute may be specified to say that if one drawer
    is opened then all others with the same key must be closed. On
    initialization, the first drawer in each set is opened by default.

--- src/app/drawer.js
+++ src/app/drawer.js
@@ -7,20 +7,83 @@ function getDrawer(opener, attr) {
   return drawer;
 }
 
+function getDrawerTriggers(drawer) {
+  var drawerName = drawer.attr('data-drawer'),
+      triggers = [];
+  if ( drawerName ) {
+    triggers = triggers.concat($(
+      '[data-open-drawer="'+drawerName+'"]'+
+      ',[data-close-drawer="'+drawerName+'"]'+
+      ',[data-toggle-drawer="'+drawerName+'"]'
+    ).toArray());
+  }
+  triggers = triggers.concat(
+    drawer.find('[data-drawer=""]')
+    .filter(function(trigger) {
+      return getDrawer(trigger)[0] === drawer[0];
+    })
+    .toArray()
+  );
+  return $(triggers);
+}
+
+function openDrawer(drawer) {
+  drawer.addClass('is-open');
+  getDrawerTriggers(drawer).addClass('is-open');
+  
+  var drawerKey = drawer.attr('data-drawer-key');
+  if ( drawerKey ) {
+    $('[data-drawer][data-drawer-key="'+drawerKey+'"]')
+      .not(drawer)
+      .each(function(i, drawer) {
+        closeDrawer($(drawer));
+      });
+  }
+}
+
+function closeDrawer(drawer) {
+  drawer.removeClass('is-open');
+  getDrawerTriggers(drawer).removeClass('is-open');
+}
+
+function toggleDrawer(drawer) {
+  if ( drawer.hasClass('is-open') ) {
+    closeDrawer(drawer);
+  } else {
+    openDrawer(drawer);
+  }
+}
+
 $(document)
   .on('click', '[data-open-drawer]', function(e) {
-    getDrawer(e.target, 'data-open-drawer').addClass('is-open');
+    openDrawer(getDrawer(e.target, 'data-open-drawer'));
   })
   .on('click', '[data-close-drawer]', function(e) {
-    getDrawer(e.target, 'data-close-drawer').removeClass('is-open');
+    closeDrawer(getDrawer(e.target, 'data-close-drawer'));
   })
   .on('click', '[data-toggle-drawer]', function(e) {
-    getDrawer(e.target, 'data-toggle-drawer').toggleClass('is-open');
+    toggleDrawer(getDrawer(e.target, 'data-toggle-drawer'));
   })
   .on('click focusin', function(e) {
     $('[data-drawer][data-drawer--auto-close].is-open')
       .not(getDrawer(e.target))
-      .removeClass('is-open');
+      .each(function(i, drawer) {
+        closeDrawer($(drawer));
+      });
   });
-  
+
+(function() {
+  var seen = {};
+  $('[data-drawer-key]').each(function() {
+    var drawer = $(this),
+        key = drawer.attr('data-drawer-key');
+    if ( key ) {
+      if ( !seen[key] ) {
+        seen[key] = true;
+        openDrawer(drawer);
+      }
+    }
+  });
+})();
+
 })();
