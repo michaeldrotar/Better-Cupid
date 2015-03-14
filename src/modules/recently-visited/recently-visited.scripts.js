f323c79711f259c014191d4fd4c66088344fc459
Michael <michaeldrotar@gmail.com>
2015-03-14 17:53:28 -0400

    Fix recently-visited to store unique users
    Was getting some urls for the same user with different query params.

--- src/modules/recently-visited/recently-visited.scripts.js
+++ src/modules/recently-visited/recently-visited.scripts.js
@@ -7,12 +7,12 @@ Module.run('recently-visited', function(module, db) {
       itemHeight = items && items.length && items[0].getHeight("outer"),
       visHeight = itemHeight && Math.floor((visRowCount * itemHeight) + (itemHeight / 3)),
       list, min;
-  
+
   if ( !recentlyVisited ) return;
-  
+
   items.reverseForEach(function(item) {
     var visit = {
-      url: item.getElementByTagName("a").href,
+      url: item.getElementByTagName("a").href.replace(/\?.+$/, ''),
       image: item.getElementByTagName("img").src
     };
     visitLog.reverseForEach(function(logItem, index, log) {
@@ -22,34 +22,34 @@ Module.run('recently-visited', function(module, db) {
     });
     visitLog.splice(0, 0, visit);
   });
-  
+
   while ( visitLog.length > 1000 ) {
     visitLog.pop();
   }
-  
+
   module.db.set("recentlyVisited", visitLog);
-  
+
   if ( visRowCount === 0 || visitLog.length === 0 ) {
     recentlyVisited.style.display = "none";
     return;
   }
-  
+
   list = recentlyVisited.getElementByTagName("ul");
   min = Math.min(visitLog.length, maxItemCount);
-  
+
   list.innerHTML = "";
-  
+
   visitLog.forEach(function(visit, index, log) {
     if ( index < min ) {
       var li = document.createElement("li"),
         a = document.createElement("a"),
         img = document.createElement("img");
-        
+
       li.appendTo(list);
-      
+
       a.href = visit.url;
       a.appendTo(li);
-      
+
       img.src = visit.image;
       img.border = 0;
       img.width = 40;
@@ -57,19 +57,19 @@ Module.run('recently-visited', function(module, db) {
       img.appendTo(a);
     }
   });
-  
+
   if ( list.getHeight() > visHeight ) {
     var holder = document.createElement("div");
     holder.insertAbove(list);
     list.appendTo(holder);
-    
+
     holder.setStyle({
       height: visHeight+"px",
       overflow: "hidden",
       position: "relative"
     });
     list.setStyle("position", "absolute");
-    
+
     holder.addEventListener("mousewheel", function(e) {
       var dir = e.wheelDelta > 0 ? 1 : -1,
         change = dir * Math.ceil(holder.getHeight() * 2/3),
@@ -79,7 +79,7 @@ Module.run('recently-visited', function(module, db) {
         newTop = top + change,
         marginTop = parseInt(list.getCssStyle("margin-top")),
         marginBottom = parseInt(list.getCssStyle("margin-bottom"));
-      
+
       if ( dir > 0 && top === marginTop ) return;
       if ( dir < 0 && top - marginTop - marginBottom === -area ) return;
       if ( newTop > 0 ) {
@@ -87,7 +87,7 @@ Module.run('recently-visited', function(module, db) {
       } else if ( newTop < -area ) {
         newTop = -area;
       }
-      
+
       list.setStyle("top", newTop+"px");
       e.stopPropagation();
       e.preventDefault();
